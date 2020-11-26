66714a37123e6865ca4ef8de0462e75dae968861
Elsayed Abdullah <sayedaladway@gmail.com>
2020-11-26 10:08:47 +0200

    finish the mobile navbar

--- js/app.js
+++ js/app.js
@@ -1,4 +1,45 @@
 $(document).ready(function () {
+  $(window).scroll(function () {
+    if ($(this).scrollTop() >= 33) {
+      $("header .navbar").addClass("nav-fixed");
+    } else {
+      $("header .navbar").removeClass("nav-fixed");
+    }
+  });
+
+  $(".navbar .toggler").click(function (event) {
+    if (!event.detail || event.detail == 1) {
+      //activate on first click only to avoid hiding again on double clicks
+      $(".navbar").toggleClass("navbar-show-mobile");
+      $(this).toggleClass("change");
+      $("body, html").toggleClass("overlay");
+      $(".navbar-collapse").toggleClass("show");
+    }
+    return false;
+  });
+
+  // when click in anywhere in document close the navbar menu and clear the overlay from the body
+  $("body").on("click", function (e) {
+    var $currEl = $(e.currentTarget);
+    if (!$currEl.is(".navbar") && !$currEl.closest(".navbar").length) {
+      $(".navbar .toggler").removeClass("change");
+      $("html,body").removeClass("overlay");
+      $(".navbar-collapse").removeClass("show");
+      $(".navbar").removeClass("navbar-show-mobile");
+    }
+  });
+  // stop propagation (closing navbar when click inside it) when click on navbar when the menu open in mobile screen
+  $(".navbar").on("click", function (e) {
+    e.stopPropagation();
+  });
+
+  // $(".faq .dropdown-menu li").on("click", function () {
+  //   $(this).addClass("active").siblings().removeClass("active");
+  //   $(".tab-content > div").hide();
+  //   $(".faq .dropdown .btn").text($(this).text());
+  //   $($(this).data("content")).fadeIn();
+  // });
+
   initFileUploader("#zdrop");
 
   function initFileUploader(target) {
